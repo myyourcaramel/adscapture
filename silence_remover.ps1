e4119164d779473c1e2337d155bdda4be0982cfd
argki <k.info.idea@gmail.com>
2024-12-28 14:18:33 +0900

    Fix:  先頭フレームから初回の無音区間までカットされてしまう現象を改善した #1

--- silence_remover.ps1
+++ silence_remover.ps1
@@ -31,7 +31,26 @@ foreach ($j in 1..$num_trial_repeat) {
     $ffmpeg_com = 'ffmpeg -i in.mp4 -filter_complex "'
     $index_process = ($index_bin_end[$j - 1] + 1)..($index_bin_end[$j])
 
-    foreach ($i in $index_process) { $ffmpeg_com = $ffmpeg_com + "[0:v]trim=" + [string]([single]$end_time[$i - 1] - 0.001) + ":" + [string]([single]$start_time[$i] + 0.001) + ",setpts=PTS-STARTPTS[v" + [string]($i - 1) + "]; [0:a]atrim=" + [string]([single]$end_time[$i - 1] - 0.001) + ":" + [string]([single]$start_time[$i] + 0.001) + ",asetpts=PTS-STARTPTS[a" + [string]($i - 1) + "]; " }
+    foreach ($i in $index_process) {
+        if ($i -eq ($index_bin_end[$j - 1] + 1)) {
+            if ($i -eq 1) {
+                $ffmpeg_com = $ffmpeg_com + "[0:v]trim=0:" + [string]([single]$start_time[0] + 0.001) + ",setpts=PTS-STARTPTS[v" + [string]($i - 1) + "]; " +
+                "[0:a]atrim=0:" + [string]([single]$start_time[0] + 0.001) + ",asetpts=PTS-STARTPTS[a" + [string]($i - 1) + "]; "
+            }
+            else {
+                $ffmpeg_com = $ffmpeg_com + "[0:v]trim=" + [string]([single]$end_time[$i - 2] - 0.001) + ":" + [string]([single]$start_time[$i - 1] + 0.001) + ",setpts=PTS-STARTPTS[v" + [string]($i - 1) + "]; " +
+                "[0:a]atrim=" + [string]([single]$end_time[$i - 2] - 0.001) + ":" + [string]([single]$start_time[$i - 1] + 0.001) + ",asetpts=PTS-STARTPTS[a" + [string]($i - 1) + "]; "
+            }
+        }
+        elseif ($i -eq $index_bin_end[$j]) {
+            $ffmpeg_com = $ffmpeg_com + "[0:v]trim=" + [string]([single]$end_time[$i - 1] - 0.001) + ",setpts=PTS-STARTPTS[v" + [string]($i - 1) + "]; " +
+            "[0:a]atrim=" + [string]([single]$end_time[$i - 1] - 0.001) + ",asetpts=PTS-STARTPTS[a" + [string]($i - 1) + "]; "
+        }
+        else {
+            $ffmpeg_com = $ffmpeg_com + "[0:v]trim=" + [string]([single]$end_time[$i - 2] - 0.001) + ":" + [string]([single]$start_time[$i - 1] + 0.001) + ",setpts=PTS-STARTPTS[v" + [string]($i - 1) + "]; " +
+            "[0:a]atrim=" + [string]([single]$end_time[$i - 2] - 0.001) + ":" + [string]([single]$start_time[$i - 1] + 0.001) + ",asetpts=PTS-STARTPTS[a" + [string]($i - 1) + "]; "
+        }
+    }
     foreach ($i in $index_process) { $ffmpeg_com = $ffmpeg_com + "[v" + [string]($i - 1) + "][a" + [string]($i - 1) + "]" }
     $num_streams = $index_process.Length
     $ffmpeg_com = $ffmpeg_com + 'concat=n=' + [string]($num_streams) + ':v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" out' + [string]$j + '.mp4'
